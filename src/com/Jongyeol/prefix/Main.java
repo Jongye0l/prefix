e8ee94fdd760ba95f3f486d8f7b2cbfdd1b72d10
Jongyeol <45934107+Jongye0l@users.noreply.github.com>
2022-06-06 22:33:47 +0900

    1.0.2
    Luckperms플러그인이 없을때 오류로그 도배 해결

--- src/com/Jongyeol/prefix/Main.java
+++ src/com/Jongyeol/prefix/Main.java
@@ -2,7 +2,9 @@ package com.Jongyeol.prefix;
 
 import com.Jongyeol.prefix.prefix.Repeat;
 import com.Jongyeol.prefix.Notification.madeby;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -15,6 +17,11 @@ public class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new EventListener(), this);
         BukkitTask Repeat = new Repeat().runTaskTimer(this, 0L, d);
         BukkitTask madeby = new madeby().runTaskTimer(this, 0L, 54000L);
+        Plugin Luckperms = Bukkit.getPluginManager().getPlugin("LuckPerms");
+        if(Luckperms == null) {
+            System.out.println("Luckperms플러그인이 감지되지 않았습니다.");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
     }
     @Override
     public void onDisable() {}
